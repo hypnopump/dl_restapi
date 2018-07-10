325810204d87c9b9dc831ddbaf34871d847f183d
Eric Alcaide <ericalcaide1@gmail.com>
2018-07-10 22:29:58 +0200

    login/logout working in beta

--- johnsnow/__init__.py
+++ johnsnow/__init__.py
@@ -2,17 +2,22 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect, url_for
+from flask import session
 
 # -*- coding: iso-8859-15 -
 from flask_cors import CORS, cross_origin
 from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask.ext.session import Session
 import os
 import json
 import hashlib
 
+
 app = Flask(__name__)
+app.secret_key = "dafuq?lolasso"
 
 @app.route('/')
 def web():
@@ -31,13 +36,33 @@ def post(id):
 def leader():
 	return render_template('leader.html')
 
+@app.route('/signup/')
+def signup():
+	return render_template('signup.html', mess=None)
+
 @app.route('/login/')
 def login():
 	return render_template('login.html')
 
-@app.route('/signup/')
-def signup():
-	return render_template('signup.html', mess=None)
+@app.route('/logger/', methods = ['GET', 'POST'])
+def logger():
+	email = request.form['email']
+	password = hashlib.sha256(bytearray(request.form['password'], "utf-8")).hexdigest()
+	if True: # if user_in_db:
+		# username = check_db_for_username
+		# user_id = check_db_for_id
+		session["username"] = "eric_alcaide" # username
+		session["email"] = email
+		session["id"] = 1 # user_id
+	return redirect(url_for('web'))
+
+@app.route('/logout/')
+def logout():
+   # remove the username, id from the session if it is there
+   session.pop('username', None)
+   session.pop('id', None)
+   session.pop('email', None)
+   return redirect(url_for('web'))
 
 @app.route('/new_user/', methods = ['GET', 'POST'])
 def new_user():
